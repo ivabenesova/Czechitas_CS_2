441c85798242e7b35dfd1676ad4d13f710880c0e
Benešová, Iva <iva.benesova@thermofisher.com>
2024-05-26 21:53:56 +0200

    ukol007

---
+++ 07_ukol/Text.cs
@@ -0,0 +1,18 @@
+﻿namespace _07_ukol;
+
+internal class Text : GraphicObject
+{
+    public string TextContent { set; get; }
+
+
+    public Text(string text, ConsoleColor color) : base(color)
+    {
+        TextContent = text;
+    }
+
+    public override void Draw()
+    {
+        base.Draw();
+        Console.WriteLine(TextContent);
+    }
+}
---
+++ 07_ukol/Triangle.cs
@@ -0,0 +1,20 @@
+﻿namespace _07_ukol;
+
+internal class Triangle : GraphicObject
+{
+    public int Side { set; get; }
+
+    public Triangle(int sideLength, ConsoleColor color) : base(color)
+    {
+        Side = sideLength;
+    }
+
+    public override void Draw()
+    {
+        base.Draw();
+        for (int i = 0; i < Side; i++)
+        {
+            Console.WriteLine(new string('#', (Side - i)));
+        }
+    }
+}
