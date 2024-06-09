0f6a3bce74523c727cb36fcc5cc1a8981e454a1b
Benešová, Iva <iva.benesova@thermofisher.com>
2024-06-09 20:36:03 +0200

    test image processing

--- project/project/ImageProcessing.cs
+++ project/project/ImageProcessing.cs
@@ -4,44 +4,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-using System.IO;
+using System.Drawing.Text;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace project; 
+
+namespace project;
+
 internal class ImageProcessing
 {
     private Bitmap _image;
-    
-    public string FilePath { get; set; }
-    public Bitmap Image { get; set; }
 
-    private string basePath = AppDomain.CurrentDomain.BaseDirectory;
+    public string FilePath { get; set; }
+    public Mat Image { get; set; }
+
+    private string _basePath = AppDomain.CurrentDomain.BaseDirectory;
 
     public ImageProcessing(string filePath)
     {
-        FilePath = Path.GetFullPath(Path.Combine(basePath, filePath));
-    try
-
-    {
-            using (Bitmap image = new Bitmap(FilePath))
-            {
-
-                Console.WriteLine($"Image with dimensions {image.Width}x{image.Height}, format: {image.PixelFormat} succesfully loaded.");
-            }
+        FilePath = Path.GetFullPath(Path.Combine(_basePath, filePath));
+        Console.WriteLine(FilePath);
+        try
+        {
+            Image = Cv2.ImRead(FilePath);
+            Console.WriteLine($"Image with dimensions {Image.Width}x{Image.Height} succesfully loaded.");
+            ;
         }
         catch (Exception e)
         {
-            Console.WriteLine("Došlo k chybě při načítání obrázku: " + e.Message);
+            Console.WriteLine("Can not load image: " + e.Message);
         }
     }
 
+    public void ImageTransformations() //in progress
+    {
+        using (Image = Cv2.ImRead(FilePath))
+        {
+            // to black and white
+            Mat grayImg = new Mat();
+            Cv2.CvtColor(Image, grayImg, ColorConversionCodes.BGR2GRAY);
 
+            //Gaussian blur 
+            Mat blurred = new Mat();
+            Cv2.GaussianBlur(grayImg, blurred, new OpenCvSharp.Size(5, 5), 0);
 
+            //detect edges
+            Mat edges = new Mat();
+            Cv2.Canny(blurred, edges, 50, 150);
+
+           
+
+           
+            /////////////////////////// chatgpt magic
+
+            // Zobrazení výsledného obrazu
+            Cv2.ImShow("Detected Rectangles", Image);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+
+            
+            SaveImage(Image, "result.jpg");
+        }
+    }
+
+    
+
+    public void SaveImage(Mat imageToSave, string imageName)
+    {   
         
+        string outputPath = Path.Combine(@"..\..\..\output_images\", imageName);
+        string PathToSave = Path.GetFullPath(Path.Combine(_basePath, outputPath));
 
-    
-
-     
-    
-
+        Cv2.ImWrite(PathToSave, imageToSave);
+    }
 }
 
--- project/project/Program.cs
+++ project/project/Program.cs
@@ -2,6 +2,9 @@
 using System.Drawing;
 using System.IO;
 using static System.Net.Mime.MediaTypeNames;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using Image = System.Drawing.Image;
 
 namespace project;
 
@@ -10,41 +13,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        string relativePath = @"..\..\..\Resources\kingdomino.jpg";
-        
+        string relativePath = @"..\..\..\Resources\kingdomino1.jpg";
+
         ImageProcessing imageProcessor = new ImageProcessing(relativePath);
 
-        int[,] kingdomLandscapesArray = new int[5, 5]
-        {
-            {1,1,2,2,3},
-            {1,1,2,2,3},
-            {4,4,6,6,3},
-            {1,1,2,6,2},
-            {1,1,2,2,3}
-        };
+        imageProcessor.ImageTransformations();
+       
 
-        int[,] kingdomCrownsArray = new int[5, 5]
-        {
-            {0,0,1,1,0},
-            {0,0,0,0,1},
-            {1,1,0,1,1},
-            {0,0,1,1,0},
-            {0,0,0,0,1}
-        };
+        //int[,] kingdomLandscapesArray = new int[5, 5]
+        //{
+        //    {1,1,2,2,3},
+        //    {1,1,2,2,3},
+        //    {4,4,6,6,3},
+        //    {1,1,2,6,2},
+        //    {1,1,2,2,3}
+        //};
 
-        KingdominoGrid kingdom1 = new KingdominoGrid(kingdomLandscapesArray, kingdomCrownsArray);
+        //int[,] kingdomCrownsArray = new int[5, 5]
+        //{
+        //    {0,0,1,1,0},
+        //    {0,0,0,0,1},
+        //    {1,1,0,1,1},
+        //    {0,0,1,1,0},
+        //    {0,0,0,0,1}
+        //};
+
+        //KingdominoGrid kingdom1 = new KingdominoGrid(kingdomLandscapesArray, kingdomCrownsArray);
 
 
-        Dictionary<int, Tuple<int, int>> clusters = kingdom1.FindClustersAndReturnDictWithClusterSizesAndCrownNumbers();
+        //Dictionary<int, Tuple<int, int>> clusters = kingdom1.FindClustersAndReturnDictWithClusterSizesAndCrownNumbers();
 
-        Console.WriteLine($"výsledek: {kingdom1.CalculateResult(clusters)}");
+        //Console.WriteLine($"výsledek: {kingdom1.CalculateResult(clusters)}");
 
-        foreach (KeyValuePair<int, Tuple<int, int>> item in clusters)
-        {
-            {
-                Console.WriteLine($"cluster no. {item.Key}: size {item.Value.Item1},  crowns {item.Value.Item2}");
-            }
-        }
+        //foreach (KeyValuePair<int, Tuple<int, int>> item in clusters)
+        //{
+        //    {
+        //        Console.WriteLine($"cluster no. {item.Key}: size {item.Value.Item1},  crowns {item.Value.Item2}");
+        //    }
+        //}
     }
 }
 
